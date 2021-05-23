4d4215aeebd534a5d53881e50ca11fa5fa8d3408
Giorgi <giorgi@giorgi.dev>
2021-05-23 23:05:24 +0400

    Change login/logout callback scheme

--- Movie Explorer/Movie Explorer.Android/LoginCallbackInterceptorActivity.cs
+++ Movie Explorer/Movie Explorer.Android/LoginCallbackInterceptorActivity.cs
@@ -6,7 +6,7 @@ using Okta.Xamarin.Android;
 namespace Movie_Explorer.Droid
 {
     [Activity(Label = "LoginCallbackInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleInstance)]
-    [IntentFilter(actions: new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "com.okta.dev-7462271" }, DataPath = "/callback")]
+    [IntentFilter(actions: new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "com.okta.dev-7462271.login" }, DataPath = "/callback")]
     public class LoginCallbackInterceptorActivity : OktaLoginCallbackInterceptorActivity<MainActivity>
     {
     }
--- Movie Explorer/Movie Explorer.Android/LogoutCallbackInterceptorActivity.cs
+++ Movie Explorer/Movie Explorer.Android/LogoutCallbackInterceptorActivity.cs
@@ -6,7 +6,7 @@ using Okta.Xamarin.Android;
 namespace Movie_Explorer.Droid
 {
     [Activity(Label = "LogoutCallbackInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleInstance)]
-    [IntentFilter(actions: new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "com.okta.dev-7462271" })]
+    [IntentFilter(actions: new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "com.okta.dev-7462271.logout" }, DataPath = "/callback")]
     public class LogoutCallbackInterceptorActivity : OktaLogoutCallbackInterceptorActivity<MainActivity>
     {
     }
